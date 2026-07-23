09b3867d054f18e9c8956ee972066e0d75add566
Muhammad Usman <usman.rauf.953@gmail.com>
2026-07-24 04:49:58 +0500

    server update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tinyurl-frontend-vert.vercel.app",
+  }),
+);
 app.use(express.json());
 
 app.use("/", URLRoute);
