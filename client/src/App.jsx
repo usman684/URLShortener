04353e0096b497e5fe98d07bb60e3622db9f7f46
Muhammad Usman <usman.rauf.953@gmail.com>
2026-07-24 04:43:18 +0500

    update client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Component/Navbar/Navbar.jsx";
 import Hero from "./Component/Hero/Hero.jsx";
-import Accordion from "./Component/Accordian/Accordian.jsx";
+import Accordion from "./Component/Accordion/Accordion.jsx";
 import Footer from "./Component/Footer/Footer.jsx";
 
 const App = () => {
