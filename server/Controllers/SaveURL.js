b06c16fb31190e25024ba4f5ac2ce201b9c4e21b
Muhammad Usman <usman.rauf.953@gmail.com>
2026-07-24 05:25:18 +0500

    server update

--- server/Controllers/SaveURL.js
+++ server/Controllers/SaveURL.js
@@ -7,7 +7,7 @@ export const SaveURL = async (req, res) => {
     const shortId = generateShortId(7);
     const newURL = new URLs({ longUrl: longUrl, shortId: shortId });
     await newURL.save();
-    const shortURL = `https://tinyurlserver-production.up.railway.app/${shortId}`;
+    const shortURL = `${process.env.BASE_URL}/${shortId}`;
     res.status(200).json({
       ok: true,
       shortURL: shortURL,
